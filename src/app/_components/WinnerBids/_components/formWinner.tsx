bb062023e137e6d153bc46bab65b00d72c952afb
guilhermemoriggidesouza <guilhermemoriggidesouza@gmail.com>
2024-03-14 17:23:25 -0300

    fix: input

--- src/app/_components/WinnerBids/_components/formWinner.tsx
+++ src/app/_components/WinnerBids/_components/formWinner.tsx
@@ -8,8 +8,9 @@ export default async () => {
     return (
         <>
             <label htmlFor="bid">Cadastre aqui os ultimos jogos ganhadores </label>
-            <Input id="bid" type="text" name="bid" />
-            <small>Informe o jogo separando por &quot;,&quot; os numeros, ex: 1,2,3,11,12,13... </small>
+            <Input id="bid" type="text" name="bid" placeholder="1,2,3,4,5,6" />
+            <small>- Informe o jogo separando por &quot;,&quot; os numeros, ex: 1,2,3,11,12,13... </small>
+            <small>- Informe um jogo de cada vez</small>
             <div className={styles.button}>
                 <Button disabled={data.pending} type="submit">{data.pending ? "Carregando..." : "Salvar"}</Button>
             </div>
